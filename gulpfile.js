83d61cebb2eb72d43de67f62808e32e9318ebc92
Amit Kumar <email@amitkumar.com>
2021-05-24 20:36:53 -0400

    bump gulp and node-sass versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
 
-gulp.task('bundle', function() {
-  gulp.src('./src/carousel.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('./dist/js/'));
-    
-  gulp.src('./src/scss/*.scss')
-    .pipe(gulp.dest('./dist/scss/'));
+gulp.task("bundle", (done) => {
+  gulp
+    .src("./src/carousel.js")
+    .pipe(
+      babel({
+        presets: ["es2015"],
+      })
+    )
+    .pipe(gulp.dest("./dist/js/"));
+
+  gulp.src("./src/scss/*.scss").pipe(gulp.dest("./dist/scss/"));
+
+  done();
 });
